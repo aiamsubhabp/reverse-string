446a674802fa4d4a5c0e8a868b767abc3d40b0ee
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-05-10 00:30:44 -0400

    solved

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,14 @@
 function reverseString(str) {
   // type your code here
+  let revStr = ''
+
+  for (let i = str.length -1 ; i >= 0; i --) {
+    revStr += str[i]
+  }
+  return revStr
 }
+
+console.log(reverseString('Butts'))
 
 if (require.main === module) {
   // add your own tests in here
